980f2fc51e5e6f6e6eb9d1ae85a65f81f722af28
Daisuke Sakurai <d.sakurai0@gmail.com>
2024-05-03 11:49:51 +0900

    Initial Commit

---
+++ New Safari Window/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  New Safari Window
+//
+//  Created by Daisuke Sakurai on 2024/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ New Safari Window/New_Safari_WindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Safari_WindowApp.swift
+//  New Safari Window
+//
+//  Created by Daisuke Sakurai on 2024/05/03.
+//
+
+import SwiftUI
+
+@main
+struct New_Safari_WindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
